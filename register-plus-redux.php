3a288adb671ce928e93a68c53d3f99dbc4de6972
tanaylakhani <tanaylakhani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-06 18:45:11 +0000

    ReadyGraph extension added
    git-svn-id: https://plugins.svn.wordpress.org/register-plus-redux/trunk@1002721 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- register-plus-redux.php
+++ register-plus-redux.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Author: radiok
+Author: radiok, Tanay Lakhani
 Plugin Name: Register Plus Redux
 Author URI: http://radiok.info/
 Plugin URI: http://radiok.info/blog/category/register-plus-redux/
 Description: Enhances the user registration process with complete customization and additional administration options.
-Version: 3.9.12
+Version: 4.0.0
 Text Domain: register-plus-redux
 Domain Path: /languages
 */
@@ -26,7 +26,7 @@ Domain Path: /languages
 // TODO: Enhancement- Alter admin pages to match registration/signup
 // TODO: Enhancement- Widget is lame/near worthless
 
-define( 'RPR_VERSION', '3.9.11' );
+define( 'RPR_VERSION', '4.0.0' );
 define( 'RPR_ACTIVATION_REQUIRED', '3.9.6' );
 
 if ( !class_exists( 'Register_Plus_Redux' ) ) {
@@ -58,6 +58,7 @@ if ( !class_exists( 'Register_Plus_Redux' ) ) {
 			global $wp_roles;
 			add_role( 'rpr_unverified', 'Unverified' );
 			update_option( 'register_plus_redux_last_activated', RPR_ACTIVATION_REQUIRED );
+			add_option('rg_rpr_plugin_do_activation_redirect', true);
 		}
 
 		public static /*.void.*/ function rpr_uninstall() {
@@ -615,9 +616,44 @@ if ( !class_exists( 'Register_Plus_Redux' ) ) {
 		public /*.string.*/ function rpr_filter_mail_content_type_html( /*.string.*/ $content_type ) {
 			return 'text/html';
 		}
+		public function rpr_rrmdir($dir) {
+			  if (is_dir($dir)) {
+				$objects = scandir($dir);
+				foreach ($objects as $object) {
+				  if ($object != "." && $object != "..") {
+					if (filetype($dir."/".$object) == "dir") 
+					   rpr_rrmdir($dir."/".$object); 
+					else unlink   ($dir."/".$object);
+				  }
+				}
+				reset($objects);
+				rmdir($dir);
+			  }
+			  $del_url = plugin_dir_path( __FILE__ );
+			  unlink($del_url.'/readygraph-extension.php');
+			 $setting_url="admin.php?page=settings";
+			  echo'<script> window.location="'.admin_url($setting_url).'"; </script> ';
+		}
+		public function rpr_delete_rg_options() {
+			delete_option('readygraph_access_token');
+			delete_option('readygraph_application_id');
+			delete_option('readygraph_refresh_token');
+			delete_option('readygraph_email');
+			delete_option('readygraph_settings');
+			delete_option('readygraph_delay');
+			delete_option('readygraph_enable_sidebar');
+			delete_option('readygraph_auto_select_all');
+			delete_option('readygraph_enable_notification');
+			delete_option('readygraph_enable_branding');
+			delete_option('readygraph_send_blog_updates');
+			delete_option('readygraph_send_real_time_post_updates');
+			delete_option('readygraph_popup_template');
+		}
 	}
 }
-
+if( file_exists(plugin_dir_path( __FILE__ ).'/readygraph-extension.php' ) ) {
+		include "readygraph-extension.php";
+}
 // include secondary php files outside of object otherwise $register_plus_redux will not be an instance yet
 if ( class_exists( 'Register_Plus_Redux' ) ) {
 	//rumor has it this may need to declared global in order to be available at plugin activation
